75d4356fd9a5964c56b6a07bd891973480e6e462
Vansh Verma <99641079+vanshverma01@users.noreply.github.com>
2022-02-21 22:58:43 -0600

    Update Container With Most Water.py

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -11,3 +11,18 @@ class Solution:
                 res.append(height[r] * (r-l))
                 r -= 1                     
         return max(res)
+#resolved because ij forgot how to leetcode :/
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        current = 0
+        l,r = 0, len(height) - 1
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            if area > current:
+                current = area
+            if height[r] < height[l]:
+                r -= 1
+            else:
+                l += 1
+        return current
+   #first solution is less efficient but both are very good
