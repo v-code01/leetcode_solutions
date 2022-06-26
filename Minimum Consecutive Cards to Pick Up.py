6a2d9756315009ffdf7687ef1a3ebc5106a6dda5
Vansh Verma <99641079+vverma-code@users.noreply.github.com>
2022-06-26 12:43:40 -0500

    Create Minimum Consecutive Cards to Pick Up.py

---
+++ Minimum Consecutive Cards to Pick Up.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        def f(a):
+            last = {}
+            for i, v in enumerate(a):
+                if v in last:
+                    yield i - last[v] + 1
+                last[v] = i
+
+        return min(f(cards), default=-1)
+     #IM BACK
